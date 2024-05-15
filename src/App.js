e405b0aecda522109d3ae1ecb952984084b45eda
lucascorreaa <lucas.lca14@gmail.com>
2024-05-14 22:00:21 -0300

    feat: created react course repository

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="card">
+        <h1>React Course</h1>
+        <p>Front-End Javascript</p>
+      </div>
+    </body>
   );
 }
 
