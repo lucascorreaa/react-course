fb233c5d12f43f1e9f598522c96571a10bd53945
lucascorreaa <lucas.lca14@gmail.com>
2024-05-21 21:43:32 -0300

    feat: created Section of Team component

---
+++ src/components/Team/src/components/section.jsx
@@ -0,0 +1,12 @@
+import "../../styles.css"
+
+export default function Section() {
+    return (
+       <div className="section-container">
+            <h3>Tagline</h3>
+            <h1>Our team</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+       </div>
+    )
+}
+
