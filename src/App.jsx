4066299ab5a095d963bffb2b1c768b7c2d9e4cff
lucascorreaa <lucas.lca14@gmail.com>
2024-05-21 22:05:45 -0300

    feat: created Main of Team component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import "./App.css"
 
-import Layout from "./components/Layout";
-import SuporteLogo from "./assets/images/svg/suporte-logo.jpeg"
+import Content from "./components/Team/src/components/content";
+import Main from "./components/Team/src/components/main";
+import Section from "./components/Team/src/components/section";
 
 function App() {
   return (
     <body>
-
-      <Layout 
-        tag="react" 
-        title="Curso React - Senai" 
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat." 
-        buttonLeftTitle="Leia mais" 
-        buttonRightTitle="Documentação" 
-        image={SuporteLogo} 
-        imageAlt="logo da suporte de domingo"
-      />
-      
+      <Section />
+      <Content />
+      <Main />
     </body>
   );
 }
---
+++ src/components/Team/src/components/main.jsx
@@ -0,0 +1,11 @@
+import "../../styles.css"
+
+export default function Main() {
+    return (
+        <div className="main-container">
+            <h1>We’re hiring!</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            <button>Open positions</button>
+        </div>
+    )
+}
