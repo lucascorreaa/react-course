232b69a7c5593c933e190e588c2e88b34ad88d4a
lucascorreaa <lucas.lca14@gmail.com>
2024-05-23 21:12:35 -0300

    refactor: added dynamic props of Team Content

--- src/components/Team/src/pattern/composition/team-content.composition.jsx
+++ src/components/Team/src/pattern/composition/team-content.composition.jsx
@@ -2,25 +2,25 @@ import '../../../styles.css'
 
 import { Dribble, EmptyImage, Linkedin, X } from "../../assets/images"
 
-export default function Content() {
+export default function Content(props) {
     return (
         <div className='content-container'>
-            <img src={EmptyImage} alt="" className='content-image' />
+            <img src={props.image ? props.image : EmptyImage} alt="" className='content-image' />
             <div className="content-title">
-                <h1>Full name</h1>
-                <h2>Job title</h2>
+                <h1>{props.name}</h1>
+                <h2>{props.role}</h2>
             </div>
             <p className='content-description'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.
+            {props.description}
             </p>
             <div className="content-icons">
-                <a href="https://br.linkedin.com/" target='_blank'>
+                <a href={props.linkedin} target='_blank'>
                     <img src={Linkedin} alt="" />
                 </a>
-                <a href="https:/x.com/" target='_blank'>
+                <a href={props.x} target='_blank'>
                     <img src={X} alt="" />
                 </a>
-                <a href="https://dribble.com/" target='_blank'>
+                <a href={props.dribble} target='_blank'>
                     <img src={Dribble} alt="" />
                 </a>    
             </div>
--- src/scripts/nomenclature.js
+++ src/scripts/nomenclature.js
@@ -1,3 +1,22 @@
+// tipos de dados 
+
+const name = "Lucas"
+const age = 20
+const isAprovved = false
+const shoes = {
+    number: 39,
+    brand: "nike",
+    since: 2018
+} 
+const numbers = [1, 2, 3, 4, 5]
+
+console.log(name.length, "valor de caracteres")
+
+const num = numbers.map((number) => {
+    return number * 2
+})
+
+console.log(num)
 // nomenclatura de variavél
 
 const userName = "Lucas Correa"
