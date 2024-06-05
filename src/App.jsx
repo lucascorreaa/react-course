85a5eb63e6a327380f17c3a956f43a6be2d7403e
lucascorreaa <lucas.lca14@gmail.com>
2024-06-04 22:13:31 -0300

    Added <Headline /> and <Titulo /> components to the App
    Co-authored-by: samuelleitee <programmer.and.investor@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css"
+import Titulo from "./styles/css-modules/components/Titulo"
+import Headline from "./styles/css-modules/components/Navbar"
 import Square from "./styles/styled-component/components/Square";
 
 function App() {
   return (
     <body>
-      <Square />
+      <Headline />
+      <Titulo />
+      <Square />  
     </body>
   );
 }
