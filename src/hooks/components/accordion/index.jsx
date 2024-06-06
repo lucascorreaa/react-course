f73da24fa4f53e6429cdfa2a2df7c5aa97af568f
lucascorreaa <lucas.lca14@gmail.com>
2024-06-05 21:18:48 -0300

    feat: added Accordion content

--- src/hooks/components/accordion/index.jsx
+++ src/hooks/components/accordion/index.jsx
@@ -1,14 +1,37 @@
-import { AccordionButton, AccordionContainer, AccordionImage, AccordionText } from "./style";
+import {
+  AccordionButton,
+  AccordionContainer,
+  AccordionContent,
+  AccordionImage,
+  AccordionText,
+  AccordionWrapper,
+  ContentText,
+} from "./style";
 
-import { AngleUp } from "./src/svg/index"
+import { AngleUp } from "./src/svg/index";
 
 export default function Accordion() {
-    return (
-        <AccordionContainer>
-            <AccordionText>What is an accordion component?</AccordionText>
-            <AccordionButton>
-                <AccordionImage src={AngleUp} />
-            </AccordionButton>
-        </AccordionContainer>
-    )
+  const isVisible = true;
+
+  return (
+    <AccordionWrapper>
+      <AccordionContainer>
+        <AccordionText>What is an accordion component?</AccordionText>
+        <AccordionButton>
+          <AccordionImage src={AngleUp} />
+        </AccordionButton>
+      </AccordionContainer>
+
+      {isVisible && (
+        <AccordionContent>
+          <ContentText>
+            An accordion menu is a vertically stacked list of headers that can
+            be clicked to reveal or hide content associated with them. It is one
+            of many ways you can expose content to users in a progressive
+            manner.
+          </ContentText>
+        </AccordionContent>
+      )}
+    </AccordionWrapper>
+  );
 }
--- src/hooks/components/accordion/style.js
+++ src/hooks/components/accordion/style.js
@@ -1,5 +1,23 @@
 import styled from "styled-components";
 
+export const AccordionWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+export const AccordionContent = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 16px;
+    width: 300px;
+`
+
+export const ContentText = styled.p`
+    font-size: 14px;
+`
 
 export const AccordionContainer = styled.div`
     width: 300px;
