a5cad37f275e7a02f1a05931f7b6fb595af30540
lucascorreaa <lucas.lca14@gmail.com>
2024-06-06 22:21:05 -0300

    feat: added DropdownMenu in Navbar component

--- src/hooks/components/Navbar/index.jsx
+++ src/hooks/components/Navbar/index.jsx
@@ -1,6 +1,30 @@
+import { useState } from "react"
 import * as S from "./style"
+import DropdownMenu from "../DropdownMenu"
 
 export default function Navbar() {
+
+    const [open, setOpen] = useState(true)
+
+    const listPages = [
+        {
+          title: "Page one",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+        },
+        {
+          title: "Page one",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+        },
+        {
+          title: "Page one",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+        },
+        {
+          title: "Page one",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+        },
+      ];
+
     return (
         <S.Container>
             <S.Logo>Logo</S.Logo>
@@ -9,11 +33,14 @@ export default function Navbar() {
                 <S.Link>Link 2</S.Link>
                 <S.Link>Link 3</S.Link>
                 <S.Link>Link 4</S.Link>
+                {
+                    open && <DropdownMenu pages={listPages} /> 
+                }
                 <S.ContainerButton>
                     <S.Button>Button</S.Button>
                     <S.Button bgColor="black">Button</S.Button>
                 </S.ContainerButton>
             </S.Nav>
-        </S.Container>
+        </S.Container>  
     )
 }
