c1470a47e42711c422af2f55f50a45dacdfd33b3
lucascorreaa <lucas.lca14@gmail.com>
2024-06-06 21:06:50 -0300

    feat: added dynamic props in DropdownMenu component

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,29 @@ import DropdownMenu from "./hooks/components/DropdownMenu";
 
 
 function App() {
+
+      const pages = [
+        {
+            title: "Page one",
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing."
+        },
+        {
+            title: "Page one",
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing."
+        },
+        {
+            title: "Page one",
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing."
+        },
+        {
+            title: "Page one",
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing."
+        },
+      ]
+
   return (
     <body>
-      <DropdownMenu />
+      <DropdownMenu pages={pages} />
     </body>
   );
 }
--- src/hooks/components/DropdownMenu/index.jsx
+++ src/hooks/components/DropdownMenu/index.jsx
@@ -1,16 +1,35 @@
 import Relume from "./src/svg/Relume.svg"
 import * as S from "./style"
 
-export default function DropdownMenu() {
+export default function DropdownMenu(props) {
+
+
     return (
         <S.Container>
-            <S.Content>
-                <S.Image src={Relume} />
-                <S.ContentText>
-                    <S.Title>Page one</S.Title>
-                    <S.Description> Lorem ipsum dolor, sit amet consectetur adipisicing. </S.Description>
-                </S.ContentText>
-            </S.Content>
+
+        {
+          props.pages && ( props.pages > props.pages?.length == 0 )
+            
+            ? 
+            
+            props.pages.map((pages, key) => {
+                return (
+                    <S.Content key={key}>
+                        <S.Image src={Relume} />
+                        <S.ContentText>
+                            <S.Title>{pages.title}</S.Title>
+                            <S.Description>{pages.description}</S.Description>
+                        </S.ContentText>
+                    </S.Content>
+                )
+            }) 
+
+            :
+
+            <p>Coloque um Array!!</p>
+
+        }
+
         </S.Container>
     )
 }
