2c267bc6b866dd457e1229e2e68c51faa763c7e5
lucascorreaa <lucas.lca14@gmail.com>
2024-06-06 22:23:18 -0300

    feat: added absolut position style in the DropdownMenu

--- src/hooks/components/DropdownMenu/style.js
+++ src/hooks/components/DropdownMenu/style.js
@@ -1,6 +1,9 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
+    position: absolute;
+    top: 60px;
+    right:140px;
     width: 408px;
     height: auto;
     padding: 24px;
