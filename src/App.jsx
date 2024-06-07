a35287250a650eb9978dfe15eee89eb7c4847620
lucascorreaa <lucas.lca14@gmail.com>
2024-06-06 21:56:36 -0300

    feat: clean project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import "./App.css"
+import "./App.css";
 import DropdownMenu from "./hooks/components/DropdownMenu";
-
+import Navbar from "./hooks/components/Navbar";
 
 function App() {
-
-      const pages = [
-        {
-            title: "Page one",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing."
-        },
-        {
-            title: "Page one",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing."
-        },
-        {
-            title: "Page one",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing."
-        },
-        {
-            title: "Page one",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing."
-        },
-      ]
+  const pages = [
+    {
+      title: "Page one",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+    },
+    {
+      title: "Page one",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+    },
+    {
+      title: "Page one",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+    },
+    {
+      title: "Page one",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+    },
+  ];
 
   return (
     <body>
-      <DropdownMenu pages={pages} />
+      <Navbar />
+      {/* <DropdownMenu pages={pages} /> */}
     </body>
   );
 }
 
 export default App;
-
-
