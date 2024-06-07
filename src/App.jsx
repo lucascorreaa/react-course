17a6d86d51b5995dfd7b4bc6ea0ca349cecbe618
lucascorreaa <lucas.lca14@gmail.com>
2024-06-06 22:23:38 -0300

    feat: clean project

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
 import "./App.css";
-import DropdownMenu from "./hooks/components/DropdownMenu";
 import Navbar from "./hooks/components/Navbar";
 
 function App() {
-  const pages = [
-    {
-      title: "Page one",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
-    },
-    {
-      title: "Page one",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
-    },
-    {
-      title: "Page one",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
-    },
-    {
-      title: "Page one",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
-    },
-  ];
 
   return (
     <body>
       <Navbar />
-      {/* <DropdownMenu pages={pages} /> */}
     </body>
   );
 }
