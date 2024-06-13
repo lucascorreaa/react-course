b89ff3893003b564205c65a45bc04a2c5d575d08
lucascorreaa <lucas.lca14@gmail.com>
2024-06-12 21:25:34 -0300

    feat: created the Home component with tailwindcss

---
+++ src/styles/tailwindcss/components/Home/index.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main>
+      <section className="shadow-2xl shadow-black mt-28 w-96 h-96 flex flex-col items-center justify-center bg-blue-200">
+        <h1 className="text-6xl text-black-900">React Course</h1>
+        <h2 className="mt-4 text-2xl">by Suporte de Domingo</h2>
+      </section>
+      <p className="text-end mt-4 text-purple-800 underline underline-offset-8">
+        Rei do Tigrinho
+      </p>
+    </main>
+  );
+}
